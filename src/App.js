58a1634e7dd80710831d380d847e6e879c1c6543
Alex <alexandre.diguida@gmail.com>
2021-01-17 15:16:55 +0000

    react-spring for mesh animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from "react";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { Canvas, useThree, extend, useFrame } from "react-three-fiber";
+import { a, useSpring } from "react-spring/three";
 
 import "./App.css";
 
@@ -12,8 +13,12 @@ const Cube = (props) => {
   const [isBig, setIsBig] = useState(false);
   const [isHovered, setIsHover] = useState(false);
 
-  const size = isBig ? 2 : 1;
-  const color = isHovered ? "salmon" : "orange";
+  const { size, x } = useSpring({
+    size: isBig ? [2, 2, 2] : [1, 1, 1],
+    x: isBig ? 2 : 0,
+  });
+
+  const color = isHovered ? "salmon" : "pink";
 
   useFrame(() => {
     mesh.current.rotation.x += 0.01;
@@ -21,16 +26,18 @@ const Cube = (props) => {
   });
 
   return (
-    <mesh
+    <a.mesh
       {...props}
       ref={mesh}
+      scale={size}
+      position-x={x}
       onClick={() => setIsBig(!isBig)}
       onPointerOut={() => setIsHover(false)}
       onPointerOver={() => setIsHover(true)}
     >
-      <boxBufferGeometry attach="geometry" args={[size, size, size]} />
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial attach="material" {...{ color }} />
-    </mesh>
+    </a.mesh>
   );
 };
 
@@ -45,7 +52,7 @@ const Scene = () => {
       <ambientLight />
       <pointLight intensity={0.3} position={[-1, 2, 4]} />
       <Cube rotation={[10, 10, 0]} position={[0, 0, 0]} />
-      <Cube rotation={[10, 16, 0]} position={[2, 2, 0]} />
+      <Cube rotation={[10, 20, 0]} position={[2, 2, 0]} />
 
       <orbitControls args={[camera, domElement]} />
     </>
